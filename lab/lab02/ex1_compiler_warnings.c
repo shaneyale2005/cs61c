8c8fcb14dea700c8cabf030fa3d2be3c47320b11
shaneyale <zeta186012@gmail.com>
2025-09-19 00:16:28 +0800

    updated ex1_compiler_warnings.c

--- lab/lab02/ex1_compiler_warnings.c
+++ lab/lab02/ex1_compiler_warnings.c
@@ -10,9 +10,11 @@ struct Course {
 struct Course *make_course(int id, char *name) {
     struct Course *new_course = malloc(sizeof(struct Course));
     new_course->id = id;
-    new_course->name = *name;
+    // 这里的有错误
+    new_course->name = name;
 
-    return &new_course;
+    // 这里应该返回一个地址
+    return new_course;
 }
 int main() {
     struct Course *cs161 = make_course(161, "Computer Security");
